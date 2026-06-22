f98624811f087ab7baa0857588a0b4c393aa8f40
SuvarnaKumar-Winfosolutions <suvarnakumar.vasamshetty@winfosolutions.com>
2026-06-22 10:46:41 +0530

    Implement print_today_date() for PROJ1-14

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+from datetime import date
+
+
 def print_hello_world():
 	"""Print 'hello world' to stdout."""
 	print("hello world")
 
 
+def print_today_date():
+	"""Print today's date in ISO format (YYYY-MM-DD)."""
+	today = date.today()
+	print(today.isoformat())
+
+
 print_hello_world()
+print_today_date()
 
 
 def sum_two_numbers(a, b):
